3fa0e978c852ab69f87a7ee07f2298e0a8f649cc
rheiformes <rhearai77@gmail.com>
2023-03-20 12:00:35 -0500

    added template/skeleton for captcha img VC

---
+++ CaptchaImageView.swift
@@ -0,0 +1,34 @@
+//
+//  CaptchaImageView.swift
+//  IP13_Captcha
+//
+//  Created by Rai, Rhea on 3/20/23.
+//
+
+import UIKit
+
+class CaptchaImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    var correct: Bool = false
+    
+    func shake() {
+            let animation = CABasicAnimation(keyPath: "position")
+            
+            animation.duration     = 0.05
+            animation.repeatCount  = 3
+            animation.autoreverses = true
+            animation.fromValue    = NSValue(cgPoint: CGPoint(x: self.center.x - 4.0, y: self.center.y))
+            animation.toValue      = NSValue(cgPoint: CGPoint(x: self.center.x + 4.0, y: self.center.y))
+            
+            self.layer.add(animation, forKey: "position")
+        }
+
+}
--- IP13_Captcha/ViewController.swift
+++ IP13_Captcha/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var capView1: CaptchaImageView!
+    @IBOutlet var capView2: CaptchaImageView!
+    @IBOutlet var capView3: CaptchaImageView!
+    @IBOutlet var capView4: CaptchaImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func resetView() {
+        shake()
+        setImageViews()
+    }
+    
+    func shake() {
+        
+    }
+    
+    func setImageViews() {
+        pickCorrectRandomly()
+        loadImages()
+    }
+    
+    func pickCorrectRandomly() {
+        
+    }
+    func loadImages() {
+        
+    }
+    
+    //TODO: change this to use UITapGestureRecognizer since UIImageViews don't default respond to user interaction
+    @IBAction func userSelected(sender: CaptchaImageView) {
+        //verify
+        if(sender.correct) {
+            goToTransition()
+        }
+        else {
+            resetView()
+        }
+    }
+    
+    func goToTransition() {
+        
+    }
+    
+    
+    
+    
 
 }
 
